7f7761366309678c3eaad0e768903d29ceb54e85
Joal-P-Koshy <joalpkoshyy@gmail.com>
2024-02-05 19:57:26 +0530

    funny

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './components/Card';
+import Naming from './components/Naming';
+
+function App() {
+  return (
+    <>
+    <Naming/>
+    
+    </>
+  );
+}
+
+export default App;
